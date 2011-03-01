8e2a5516e558cb44631a31cc6d16828769648c52
deoxxa <deoxxa@fknsrs.biz>
2011-03-01 19:27:49 +1100

    renamed {send,recv}packets to packets-{send,recv}

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-clang -o sendpackets sendpackets.c -lm
-clang -o recvpackets recvpackets.c -lpcap
+clang -o packets-send packets-send.c -lm
+clang -o packets-recv packets-recv.c -lpcap
 clang -o generate-ips-random generate-ips-random.c
 clang -o generate-ips-lfsr generate-ips-lfsr.c
