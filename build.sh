f16b4c7040db5a0fc66be7aff1567d789dbc8d47
deoxxa <deoxxa@fknsrs.biz>
2011-03-02 23:36:31 +1100

    changed to cc for build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-clang -o packets-send packets-send.c -lpcap -lm
-clang -o packets-recv packets-recv.c -lpcap
-clang -o generate-ips-random generate-ips-random.c
-clang -o generate-ips-lfsr generate-ips-lfsr.c
+cc -o packets-send packets-send.c -lpcap -lm
+cc -o packets-recv packets-recv.c -lpcap
+cc -o generate-ips-random generate-ips-random.c
+cc -o generate-ips-lfsr generate-ips-lfsr.c
